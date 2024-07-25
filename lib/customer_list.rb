a8d192a5ddc81182ca8e278be573fa6b735fff83
Ryan Verhey <RyanVerhey@users.noreply.github.com>
2024-07-24 21:02:52 -0400

    Adding constants for ease

--- lib/customer_list.rb
+++ lib/customer_list.rb
@@ -6,6 +6,10 @@ require_relative 'customer_list/printer'
 
 # Acts as a namespace and controller for the customer list
 module CustomerList
+  FULL_NAME_SORT_FIELD = 'full_name'
+  FIRST_NAME_SORT_FIELD = 'first_name'
+  LAST_NAME_SORT_FIELD = 'last_name'
+
   def self.run(filename:, delimiter:, headers:, sort:)
     customer_list = CustomerList::CSV.new(filename:, delimiter:, headers:).read
     customer_list = CustomerList::Sorter.new(list: customer_list, sort_field: sort).sort if sort
